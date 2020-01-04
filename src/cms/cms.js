59c336c23e054084dac19e04880cb0e3c69e60c4
João Graça <joao.g.n91@gmail.com>
2020-01-04 10:06:57 -0300

    chore: remove style from pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,12 +5,9 @@ import BlogPostPreview from "./preview-templates/BlogPostPreview"
 import HomePreview from "./preview-templates/HomePreview"
 import withStyledComponentsRendered from "../helpers/styleInjector"
 
-CMS.registerPreviewStyle("./PreviewStyle.css")
+//CMS.registerPreviewStyle("./PreviewStyle.css")
 
-CMS.registerPreviewTemplate("index", withStyledComponentsRendered(HomePreview))
-CMS.registerPreviewTemplate(
-  "blog",
-  withStyledComponentsRendered(BlogPostPreview)
-)
+CMS.registerPreviewTemplate("index", HomePreview)
+CMS.registerPreviewTemplate("blog", BlogPostPreview)
 
-CMS.registerLocale("pt", pt)
+//CMS.registerLocale("pt", pt)
--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -3,9 +3,5 @@ import React from "react"
 import { Container } from "./styles"
 
 export default function() {
-  return (
-    <Container>
-      <h1>404</h1>
-    </Container>
-  )
+  return <h1>404</h1>
 }
