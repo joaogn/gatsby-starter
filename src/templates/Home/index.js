ab154171728fe36afb0756975dd5417df0f9d76a
João Graça <joao.g.n91@gmail.com>
2020-01-04 08:45:54 -0300

    chore: try deploy without staticQuery

--- src/templates/Home/index.js
+++ src/templates/Home/index.js
@@ -2,7 +2,7 @@ import React from "react"
 //import { graphql } from "gatsby"
 import { Container } from "./styles"
 
-import PostsList from "../../components/PostList"
+//import PostsList from "../../components/PostList"
 //import { useStaticQuery, graphql } from "gatsby"
 
 export function HomeTemplate({ data, isPreview }) {
@@ -18,7 +18,6 @@ export function HomeTemplate({ data, isPreview }) {
       <div className={"banner"}>
         <h1>{homeData.frontmatter.title}</h1>
         <h2>{homeData.frontmatter.subtitle}</h2>
-        <PostsList />
       </div>
     </Container>
   )
